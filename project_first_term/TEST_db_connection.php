55e7457686aa594b5a4efdbf15897638343209c1
apgallego <apgallego@users.noreply.github.com>
2021-11-22 22:46:58 +0100

    trash files removed

--- project_first_term/TEST_db_connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require 'utilities.php';
-
-    configBD('conf.xml');
-    dbConnect();
-?>
--- project_first_term/a.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $pw = 'xd';
-    $passwd = password_hash($pw, PASSWORD_DEFAULT);
-    echo '<br/>' . $passwd;
-    echo '<br/>' . password_verify('xdd', $passwd);
-?>
